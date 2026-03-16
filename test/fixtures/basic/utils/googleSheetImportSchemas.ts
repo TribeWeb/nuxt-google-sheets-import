792149b92b01b1325d74e5054e18148805b966aa
rich-tribeweb <rich@tribeweb.co.uk>
2026-03-16 01:33:07 +0000

    chore: update test

--- test/fixtures/basic/utils/googleSheetImportSchemas.ts
+++ test/fixtures/basic/utils/googleSheetImportSchemas.ts
@@ -1,9 +1,1 @@
-import { z } from 'zod'
-
-export const smokeSchema = z.object({
-  slug: z.string().min(1),
-})
-
-export const schemas = {
-  smokeSchema,
-}
+export * from './googleSheetsImportSchemas'
